76a1aa86e8d57d3ddfcab785def6d0af30501601
Atanas Mirchev <atanas.mirchev@est.fujitsu.com>
2016-04-14 15:54:23 +0200

    Create heapster addon

--- addons/run.sh
+++ addons/run.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 echo "Addon creation started"
-/opt/kube-addons/kubectl help
+WORK_DIR="/opt/kube-addons"
+cd ${WORK_DIR}
+
+# create heapster addon
+if [ "$ENABLE_CLUSTER_MONITORING" == "monasca" ]; then
+    # TODO (Atanas): sed monasca configuration and start monasca heapster
+    echo "Monasca should start"
+else
+    ./kubectl create -f heapster/standalone/
+fi
